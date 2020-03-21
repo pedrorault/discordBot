7f01e5581ba9abac2743c8391af0868f509aaf89
Pedro R Costa <pedrorault@gmail.com>
2020-03-21 00:00:45 -0300

    add option to specify 2 random pokemon

--- pokeBot/pokeBot.py
+++ pokeBot/pokeBot.py
@@ -1,9 +1,19 @@
 import requests
 import random  
 
+def choosePoke(x,y):
+    if type(x) is int and type(y) is int:
+        if x in (1,152) and y in (1,152):
+            return getPoke(x,y)
+        else:
+            return randomPoke()
+
 def randomPoke():
     x = random.randrange(1,152)
     y = random.randrange(1,152)
+    return getPoke(x,y)
+
+def getPoke(x,y):
     url = 'https://images.alexonsager.net/pokemon/fused/{0}/{0}.{1}.png'.format(x,y)
     r = requests.get(url,allow_redirects=True)
     name = 'poke.png'
