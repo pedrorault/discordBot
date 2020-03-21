709e49feee10252693c48babbe56114eefaab9b8
Pedro R Costa <pedrorault@gmail.com>
2020-03-21 00:20:28 -0300

    Revert "No scraping for waifu images, therefore no selenium ou BeautifulSoup nor Chrome install on every heroku deploy"
    This reverts commit 1804b2762da6642428d3c7c761377f2cccd8e651.

--- waifuBot/waifuBot.py
+++ waifuBot/waifuBot.py
@@ -1,8 +1,18 @@
+from bs4 import BeautifulSoup
 import requests
-import random
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options  
 
 def getWaifuFromScrapping():
-    i = random.randrange(1000,100000)
-    url = "https://www.thiswaifudoesnotexist.net/example-{}.jpg".format(i)    
-    r = requests.get(url,allow_redirects=True)
+    url = "https://www.thiswaifudoesnotexist.net/"
+    chrome_options = Options()  
+    chrome_options.add_argument("--headless")  
+    driver = webdriver.Chrome(chrome_options=chrome_options) 
+    #chromedriver already installed by Heroku, otherwise set path to chromedriver
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')    
+    imgTag = soup.find_all('img')[1] 
+    imgLink = 'http:' + imgTag['src']
+    r = requests.get(imgLink,allow_redirects=True)
     open('img.jpg','wb').write(r.content)
+    driver.close()
