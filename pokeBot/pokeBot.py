27536ea6ac8fd744e3db8c0b97dc04af1f5c3da1
Pedro R Costa <pedrorault@gmail.com>
2020-03-21 00:25:46 -0300

    ,

--- pokeBot/pokeBot.py
+++ pokeBot/pokeBot.py
@@ -1,6 +1,7 @@
 import requests
 import random  
 
+<<<<<<< HEAD
 def choosePoke(x,y):
     if type(x) is int and x in range(1,152):
         if type(y) is int and y in range(1,152):
@@ -8,12 +9,11 @@ def choosePoke(x,y):
         else:
             return randomPoke()
 
+=======
+>>>>>>> parent of 7f01e55... add option to specify 2 random pokemon
 def randomPoke():
     x = random.randrange(1,152)
     y = random.randrange(1,152)
-    return getPoke(x,y)
-
-def getPoke(x,y):
     url = 'https://images.alexonsager.net/pokemon/fused/{0}/{0}.{1}.png'.format(x,y)
     r = requests.get(url,allow_redirects=True)
     name = 'poke.png'
