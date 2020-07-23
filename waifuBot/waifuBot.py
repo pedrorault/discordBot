8ef8aa261e2aa567845584bfb13dd14ba4cda14e
Pedro R Costa <pedrorault@gmail.com>
2020-07-23 19:56:30 -0300

    fix

--- waifuBot/waifuBot.py
+++ waifuBot/waifuBot.py
@@ -8,7 +8,7 @@ def getWaifuFromScrapping():
     open('img.jpg','wb').write(r.content)
 
 def getFurry():
-    i = random.rand(0,1000000)
+    i = random.randrange(0,1000000)
     url = f'https://thisfursonadoesnotexist.com/v2/jpgs/seed{i:05}.jpg'
     r = requests.get(url,allow_redirects=True)
     open('fur.jpg','wb').write(r.content)
