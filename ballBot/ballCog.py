2603cc57d5c4aadb8443500dba016b30d456f473
Pedro R Costa <pedrorault@gmail.com>
2020-11-19 08:09:31 -0300

    simple random A~E function

--- ballBot/ballCog.py
+++ ballBot/ballCog.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from ballBot.ball import randomResponse
+import random
 
 class BallCog(commands.Cog):
     def __init__(self,bot):
@@ -9,4 +10,10 @@ class BallCog(commands.Cog):
     @commands.command(name='8ball')
     async def _8ball(self,ctx):
         msg = randomResponse()
+        await ctx.send(msg)
+    
+    @commands.command(name='random')
+    async def _random(self,ctx):
+        alternativas = ['A','B','C','D','E']
+        msg = f'Escolha {random.choice(alternativas)}'
         await ctx.send(msg)
