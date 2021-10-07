5a23317ff9e5a819ac2da353cb6fd30e186b2a51
Pedro Raul Costa <52670501+pedrorault@users.noreply.github.com>
2021-10-07 20:21:46 -0300

    Update ballCog.py

--- ballBot/ballCog.py
+++ ballBot/ballCog.py
@@ -30,4 +30,13 @@ class BallCog(commands.Cog):
         msg = f'Ainda bem que você pegou esse personagem!!! 😊😊😊'
         for i in range(3):
             await ctx.send(msg)
+    
+    @commands.command(name='vinok')
+    async def _vinok(self,ctx):
+        await ctx.send('O QUE!? EU NUNCA PEGO KAKERA')
+        await ctx.send('NÃO ACREDITO, COMO VCS PEGAM TÃO RÁPIDO?!?!?!')
+        await ctx.send('O KAKERA BRANCO SEMPRE SÓ VEM 3 ROXOS PRA MIM')
+        await ctx.send('$profile 205005381032869888')
+        await ctx.send('VAI SE FUDER MARCOS!!!')
+        await ctx.send('🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬🤬')
 
